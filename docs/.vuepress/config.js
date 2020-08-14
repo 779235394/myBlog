81e3cd729eaff9ed091175b88469fc474bef0f1e
imac <>
2020-08-14 18:38:50 +0800

    光标大小修改

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -39,7 +39,7 @@ module.exports = {
         [
             "cursor-effects",
             {
-                size: 5,                    // size of the particle, default: 2
+                size: 2,                    // size of the particle, default: 2
                 shape: ['star'],  // shape of the particle, default: 'star'
                 zIndex: 999999999           // z-index property of the canvas, default: 999999999
             }
