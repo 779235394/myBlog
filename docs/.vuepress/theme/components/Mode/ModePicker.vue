967159c0b594beb2df38686b4e4b7b1e12830acd
imac <>
2020-09-01 18:41:14 +0800

    修改语言

--- docs/.vuepress/theme/components/Mode/ModePicker.vue
+++ docs/.vuepress/theme/components/Mode/ModePicker.vue
@@ -13,90 +13,106 @@
 </template>
 
 <script>
-import applyMode from './applyMode'
+import applyMode from "./applyMode";
 export default {
-  name: 'ModeOptions',
+  name: "ModeOptions",
 
-  data () {
+  data() {
     return {
       modeOptions: [
-        { mode: 'dark', title: 'dark' },
-        { mode: 'auto', title: 'auto' },
-        { mode: 'light', title: 'light' }
+        { mode: "dark", title: "关灯" },
+        { mode: "auto", title: "自动" },
+        { mode: "light", title: "开灯" },
       ],
-      currentMode: 'auto'
-    }
+      currentMode: "auto",
+    };
   },
 
-  mounted () {
+  mounted() {
     // modePicker 开启时默认使用用户主动设置的模式
-    this.currentMode = localStorage.getItem('mode') || this.$themeConfig.mode || 'auto'
+    this.currentMode =
+      localStorage.getItem("mode") || this.$themeConfig.mode || "auto";
 
     // Dark and Light autoswitches
     // 为了避免在 server-side 被执行，故在 Vue 组件中设置监听器
-    var that = this
-    window.matchMedia('(prefers-color-scheme: dark)').addListener(() => {
-      that.$data.currentMode === 'auto' && applyMode(that.$data.currentMode)
-    })
-    window.matchMedia('(prefers-color-scheme: light)').addListener(() => {
-      that.$data.currentMode === 'auto' && applyMode(that.$data.currentMode)
-    })
+    var that = this;
+    window.matchMedia("(prefers-color-scheme: dark)").addListener(() => {
+      that.$data.currentMode === "auto" && applyMode(that.$data.currentMode);
+    });
+    window.matchMedia("(prefers-color-scheme: light)").addListener(() => {
+      that.$data.currentMode === "auto" && applyMode(that.$data.currentMode);
+    });
 
-    applyMode(this.currentMode)
+    applyMode(this.currentMode);
   },
 
   methods: {
-    selectMode (mode) {
+    selectMode(mode) {
       if (mode !== this.currentMode) {
-        this.currentMode = mode
-        applyMode(mode)
-        localStorage.setItem('mode', mode)
+        this.currentMode = mode;
+        applyMode(mode);
+        localStorage.setItem("mode", mode);
       }
     },
-    getClass (mode) {
-      return mode !== this.currentMode ? mode : `${mode} active`
-    }
-  }
-}
+    getClass(mode) {
+      return mode !== this.currentMode ? mode : `${mode} active`;
+    },
+  },
+};
 </script>
 
 <style lang="stylus">
-@require '../../styles/mode.styl'
+@require '../../styles/mode.styl';
 
-.mode-options
-  background-color var(--background-color)
+.mode-options {
+  background-color: var(--background-color);
   min-width: 125px;
   margin: 0;
   padding: 1em;
-  box-shadow var(--box-shadow);
+  box-shadow: var(--box-shadow);
   border-radius: $borderRadius;
-  .title
-    margin-top 0
-    margin-bottom .6rem
-    font-weight bold
-    color var(--text-color)
-  .color-mode-options
+
+  .title {
+    margin-top: 0;
+    margin-bottom: 0.6rem;
+    font-weight: bold;
+    color: var(--text-color);
+  }
+
+  .color-mode-options {
     display: flex;
-    flex-wrap wrap
-    li
+    flex-wrap: wrap;
+
+    li {
       text-align: center;
-      font-size 12px
-      color var(--text-color)
-      line-height 18px
-      padding 3px 6px
-      border-top 1px solid #666
-      border-bottom 1px solid #666
-      background-color var(--background-color)
-      cursor pointer
-      &.dark
-        border-radius: $borderRadius 0 0 $borderRadius
-        border-left 1px solid #666
-      &.light
-        border-radius: 0 $borderRadius $borderRadius 0
-        border-right 1px solid #666
-      &.active
+      font-size: 12px;
+      color: var(--text-color);
+      line-height: 18px;
+      padding: 3px 6px;
+      border-top: 1px solid #666;
+      border-bottom: 1px solid #666;
+      background-color: var(--background-color);
+      cursor: pointer;
+
+      &.dark {
+        border-radius: $borderRadius 0 0 $borderRadius;
+        border-left: 1px solid #666;
+      }
+
+      &.light {
+        border-radius: 0 $borderRadius $borderRadius 0;
+        border-right: 1px solid #666;
+      }
+
+      &.active {
         background-color: $accentColor;
-        color #fff
-      &:not(.active)
-        border-right 1px solid #666
+        color: #fff;
+      }
+
+      &:not(.active) {
+        border-right: 1px solid #666;
+      }
+    }
+  }
+}
 </style>
