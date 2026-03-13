37c9ebe181ddff4af0514bb38a85ce300aeff7f3
贺贺有名 <heheyouming@heheyoumingdeMacBook-Pro.local>
2026-03-13 11:53:56 +0800

    配置 GitHub Pages 自动部署

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,7 @@
 const themeConfig = require('./themeConfig.js')
 module.exports = {
     title: '葵花宝典',
+    base: '/myBlog/',
     description: '专注前端开发技术积累,积硅步,至千里',
     keywords: "贺俊兰博客", // 关键字
     evergreen: true,
